3071359285a66b5855467c38a419469b49710220
makoge boris <makogeboris@gmail.com>
2024-06-01 18:44:08 +0100

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     screens: {
-      sm: "640px",
-      lg: "1024px",
+      sm: "431px",
+      md: "640px",
+      lg: "1060px",
     },
     colors: {
       VeryDarkBlue: "hsl(243, 87%, 12%)",
@@ -15,10 +16,15 @@ module.exports = {
       LightGray: "hsl(0, 0%, 75%)",
     },
     fontFamily: {
-      sans: ["Open Sans", "sans-serif"],
+      open: ["Open Sans", "sans-serif"],
       raleway: ["Raleway", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        shadowbtn: "2px 3px 6px rgba(0, 0, 0, 0.15925)",
+        shadowbox: "0px 0px 10px 5px rgba(56, 56, 56, 0.0458843)",
+      },
+    },
   },
   plugins: [],
 };
